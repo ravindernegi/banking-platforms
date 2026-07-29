39295e4d9d8519d615f892218bb941df4cee0dd3
Ravinder Singh <r.singh.negi85@gmail.com>
2026-07-29 20:40:22 +0530

    Added api gateway route for auth and fixed ts config

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Banking Platforms Setup
+
+**Generate from inside the apps directory**
+
+`cd apps/api-gateway`
+
+`nest g co users`
+`nest g s users`
--- apps/api-gateway/src/app.service.ts
+++ apps/api-gateway/src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  healthCheck(): object {
+    return { status: 200, message: 'ok' };
   }
 }
